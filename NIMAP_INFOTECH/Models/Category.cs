603b12729f755760d72e47325631bc5a9128f987
akashshelar96 <akashshelar1155@outlook.com>
2023-03-10 10:52:35 +0530

    Category

---
+++ NIMAP_INFOTECH/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NIMAP_INFOTECH.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
