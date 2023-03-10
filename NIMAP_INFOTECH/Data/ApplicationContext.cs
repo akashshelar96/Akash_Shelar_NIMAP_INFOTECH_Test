6c95eecbeb091a1c0bfc8e17ad510fd7ebe2f3ad
akashshelar96 <akashshelar1155@outlook.com>
2023-03-10 11:00:35 +0530

    ApplicationContext.cs

--- NIMAP_INFOTECH/Data/ApplicationContext.cs
+++ NIMAP_INFOTECH/Data/ApplicationContext.cs
@@ -4,9 +4,16 @@ using System.Data;
 
 namespace NIMAP_INFOTECH.Data
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationContext(DbContextOptions<ApplicationContext>options):base(options) { }
-        public DbSet<Product>Products { get; set; }
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
+    // public ApplicationContext(DbContextOptions<ApplicationContext>options):base(options) { }
+    // public DbSet<Product>Products { get; set; }
+
 }
